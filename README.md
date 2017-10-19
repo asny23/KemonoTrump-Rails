b798a7cd56cafc77f7325d241b235a78a1a056b2
asny23 <asny23@gmail.com>
2017-10-19 23:55:39 +0900

    add usage, small fix

--- README.md
+++ README.md
@@ -11,3 +11,19 @@
 
 - カード画像: LPGL 3.0
 [Vector Playing Cards download | SourceForge.net](https://sourceforge.net/projects/vector-cards/)
+
+### 利用方法
+
+ruby 2.3
+Rails 4.2
+で動作確認をしています。
+
+    $ bundle install
+    $ echo 'development:' > config/secrets.yml
+    $ echo "  secret_key_base: $(bundle exec rake secret)" >> config/secrets.yml
+    $ rails s -b 0.0.0.0
+
+`constitute.c/ReadImage` で `not authorized` エラーが発生する場合は
+`/etc/ImageMagick/policy.xml` の
+`<policy domain="coder" rights="none" pattern="MVG" />`
+を削除またはコメントしてください。
--- app/models/card_maker.rb
+++ app/models/card_maker.rb
@@ -3,7 +3,7 @@ class CardMaker
   require 'RMagick'
   
   @@imagePath = 'app/resources/'
-  @@cardScale = 2.0;
+  @@cardScale = 2.5
 
   def make(cardId, friendsId)
     card = cardId.match(/[shdc](?:0[1-9]|1[0-3])$/)
@@ -18,8 +18,8 @@ class CardMaker
       icon = Magick::Image.read(@@imagePath + 'icon/' + friendsId + '.jpg').first
       iconWidth = icon.columns
       iconHeight = icon.rows
-      iconOffsetX = (cardWidth - iconWidth) / 2;
-      iconOffsetY = (cardHeight - iconHeight) / 2;
+      iconOffsetX = (cardWidth - iconWidth) / 2
+      iconOffsetY = (cardHeight - iconHeight) / 2
       card.resize!(@@cardScale)
       card.composite!(icon, iconOffsetX, iconOffsetY, Magick::OverCompositeOp)
       return card
